30a1ffd1bfeaa01d8c51b0bbf9d92a5c85b37b9f
sganders <sganders@ualberta.ca>
2026-06-03 17:48:32 -0600

    GREEN added getAddressByCity

--- src/main/java/city/service/Utility.java
+++ src/main/java/city/service/Utility.java
@@ -1,5 +1,16 @@
 package city.service;
-
+import salary.service.City;
+import java.util.ArrayList;
+import java.util.List;
 public class Utility {
-
+    public List<Address> getAddressesByCity(City city, String cityName) {
+        List<Address> addresses = new ArrayList<>();
+        List<Address> cityAddresses = city.listAddresses();
+        for (int i = 0; i < cityAddresses.size(); i++) {
+            if (cityAddresses.get(i).city.equals(cityName)) {
+                addresses.add(cityAddresses.get(i));
+            }
+        }
+        return addresses;
+    }
 }
